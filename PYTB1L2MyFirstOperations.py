6cdb1fc68fcfc2803cfeda7acb1908583689bd51
TenqQoe <90691256+TenqQoe@users.noreply.github.com>
2021-10-12 12:11:57 +0200

    Add files via upload

---
+++ PYTB1L2MyFirstOperations.py
@@ -0,0 +1,23 @@
+Python 3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=111
+>>> b=101
+>>> a+b
+212
+>>> a-b
+10
+>>> a*b
+11211
+>>> a/b
+1.099009900990099
+>>> a%b
+10
+>>> a+=b
+>>> b/=a
+>>> a
+212
+>>> b
+0.47641509433962265
+>>> a*b
+101.0
+>>> 
