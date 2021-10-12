d91f99dd20899c83f7dd01f36c2a03f0df84c8c7
TenqQoe <90691256+TenqQoe@users.noreply.github.com>
2021-10-12 12:43:24 +0200

    Add files via upload

---
+++ PYTB1L2HowYouLikeThemApples.py
@@ -0,0 +1,26 @@
+Python 3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> bomen=333
+>>> per=146
+>>> drkbomen=bomen/3*2
+>>> drkbomen
+222.0
+>>> lchbomen=bomen-drkbomen
+>>> lchbomen
+111.0
+>>> drkper=per*0.8
+>>> drkper
+116.80000000000001
+>>> lchgen=lchbomen*per
+>>> lchgen
+16206.0
+>>> drkgen=drkbomen*drkper
+>>> drkgen
+25929.600000000002
+>>> appels=lchgen+drkgen
+>>> appels
+42135.600000000006
+>>> pp=appels/4
+>>> pp
+10533.900000000001
+>>> 
